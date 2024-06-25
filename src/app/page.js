e1a4cccd191faea76c1be6053bdbb036d7743ad7
Surge Jackson <syackovlev@gmail.com>
2024-06-26 00:27:58 +0300

    first commit39

--- src/app/page.js
+++ src/app/page.js
@@ -2,24 +2,20 @@
 import Menu from "@/components/layout/Menu";
 import Footer from "@/components/layout/Footer";
 import {useSession} from "next-auth/react";
+import {redirect} from 'next/navigation';
 
 export default function Home() {
   const session = useSession();
   const status = session?.status;
 
-  if (status === 'authenticated') {
+  if (status === 'unauthenticated') {
+    return redirect('/login');
+  }
+
   return (
     <>
-    <Menu />
-    <Footer />
+      <Menu />
+      <Footer />
     </>
   )
-  } else 
-  {
-    return (
-      <div className="flex h-96 w-96 text-center items-center m-auto text-lg font-semibold">
-        Оформление заказов только для сотрудников компании. Пожалуйста, войдите в систему.
-      </div>
-    )
-  }
 }
