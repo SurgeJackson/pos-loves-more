d2b8dc49960bfea108e9acbe2e886ec846c737c4
Surge Jackson <syackovlev@gmail.com>
2024-07-04 20:10:58 +0300

    commit 04-07-2024_03

--- src/app/api/bot/route.js
+++ src/app/api/bot/route.js
@@ -1,6 +1,19 @@
 export const dynamic = 'force-dynamic';
 export const fetchCache = 'force-no-store';
 import { Bot, webhookCallback } from 'grammy';
+import { User } from "grammy/types";
+import { Composer } from "grammy";
+
+function getFullName(from) {
+	return from.last_name
+		? `${from.first_name} ${from.last_name}`
+		: from.first_name;
+}
+
+const composer = new Composer();
+const filterCommands = composer.filter((ctx) =>
+    ctx.chat ? ctx.chat.type === "private" : false
+  );
 
 const token = process.env.TELEGRAM_BOT_TOKEN;
 
@@ -11,6 +24,27 @@ const bot = new Bot(token);
 bot.command("start", async (ctx) => {
     await ctx.reply("Welcome! Up and running.");
   })
+
+// about user
+filterCommands.command("me", async (ctx) => {
+    const msg = `Hi ${getFullName(ctx.from)}.
+    
+    Here's a little info about yourself on telegram:
+    
+    Your name: ${getFullName(ctx.from)}
+    Your Telegram user ID: ${ctx.from.id}
+    Your Telegram username: ${ctx.from.username ? ctx.from.username : "N/A"}
+    
+  
+    ------------------------------------
+  
+    Bye Bye 👋
+    
+    `;
+  
+    await ctx.reply(msg);
+  });
+
 
 bot.on('message:text', async (ctx) => {
   await ctx.reply(ctx.message.text);
