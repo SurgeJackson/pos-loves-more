549892415297ad4b0d12795ce463d1544cef582f
Surge Jackson <syackovlev@gmail.com>
2024-07-11 00:15:31 +0300

    commit 10-07-2024_04

--- src/components/layout/MenuItemTile.js
+++ src/components/layout/MenuItemTile.js
@@ -3,7 +3,7 @@ export default function MenuItemTile({onAddToCart, item}) {
     <div className="flex flex-col items-center bg-gray-200 p-2 rounded-lg text-center
       group hover:bg-white hover:shadow-md hover:shadow-black/25 transition-all cursor-pointer relative" onClick={onAddToCart}>
       <h4 className="font-semibold text-lg pt-4">{item.name}</h4>
-      <h4 className="font-semibold text-lg my-2 text-primary">{item.basePrice.toLocaleString()}&#8381;</h4>
+      <h4 className="font-semibold text-lg my-2 text-primary">{item.basePrice.toLocaleString("ru-RU")}&#8381;</h4>
     </div>
   );
 }
