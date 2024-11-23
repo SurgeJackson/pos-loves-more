35581f2fce62963fd93f76b9a5f6452bb0c2ef17
Surge Jackson <syackovlev@gmail.com>
2024-11-23 23:40:48 +0300

    23-11-2024_01

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,12 +1,12 @@
 import {AppProvider} from "@/components/AppContext";
 import Header from "@/components/layout/Header";
-import { Roboto } from 'next/font/google'
+import { Inter } from 'next/font/google'
 import './globals.css'
 import {Toaster} from "react-hot-toast";
 import SWRProvider from '@/components/SWRProvider';
 import {PreloadResources} from '@/app/preload-resources';
 
-const roboto = Roboto({ subsets: ['latin'], weight: ['400', '500', '700'] })
+const roboto = Inter({ subsets: ['latin'], weight: ['400', '500', '700'] })
 
 export const metadata = {
   title: 'POS LOVES MORE',
@@ -17,7 +17,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en" className="scroll-smooth">
       {/* <PreloadResources /> */}
-      <body className={roboto.className}>
+      <body className={""}>
         <main className="max-w-4xl mx-auto p-2 flex flex-col">
           <AppProvider>
             <SWRProvider>
